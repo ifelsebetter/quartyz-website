f332c5aa12e7e90522478173b3feb5be0dd08f8b
ifelsebetter <tatsuhiro1808@gmail.com>
2023-08-07 17:57:35 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/main'
 import Support from './components/support';
 import Feature from './components/feature';
@@ -8,14 +8,14 @@ import Member from './components/member';
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/support' element={<Support />} />
         <Route path='/feature' element={<Feature />} />
         <Route path='/member' element={<Member />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }
 
